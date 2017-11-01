5ccb5ad473a786e82dfd32afff245f530580c447
darren <764190362@qq.com>
2017-11-01 15:50:09 +0800

    movie-filter的子组件check-filter,props属性

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres';
 
 new Vue({
     el: '#app',
@@ -19,13 +20,24 @@ new Vue({
             }
         },
         'movie-filter':{
+            data(){
+                return {
+                    genres
+                }
+            },
             template: `<div id="movie-filter">
-                           <h2>Filter results</h2>
-                            <check-filter></check-filter>
-                        </div>`,
+                    <h2>Filter results</h2>
+                    <div class="filter-group">
+                      <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>                   
+                    </div>                 
+                </div>`,
             components: {
                 'check-filter':{
-                    template: `<div>Filter</div>`
+                    props: ['title'],
+                    template: `<div class="check-filter">
+                                    <span class="checkbox"></span>
+                                    <span class="check-filter-title">{{ title }}</span>
+                                </div>`
                 }
             }
         }
