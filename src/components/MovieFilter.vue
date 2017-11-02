a853ba94157bf8293e5a45b8ed732d6cedf36efd
darren <764190362@qq.com>
2017-11-02 20:42:33 +0800

    增加时间的过滤

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,8 +1,13 @@
 <template>
     <div id="movie-filter">
         <h2>Filter results</h2>
+        <h3>By time of day</h3>
         <div class="filter-group">
-          <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>                   
+          <check-filter v-for="time in times" v-bind:title="time" v-on:check-filter="checkFilter"></check-filter>          
+        </div>   
+        <h3>By genre</h3>
+        <div class="filter-group">
+          <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>          
         </div>                 
     </div>
 </template>
@@ -11,11 +16,13 @@
 <script>
     
     import genres from '../util/genres';
+    import times from '../util/times';
     import CheckFilter from './CheckFilter.vue';
     export default {
        data(){
             return {
-                genres
+                genres,
+                times
             }
         },
         methods: {
