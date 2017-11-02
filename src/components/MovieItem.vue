fc81b21fd0fac34e7cf1503ccec35f42019816fc
darren <764190362@qq.com>
2017-11-02 14:38:25 +0800

    MovieItem通过方法来获取session的集合，即过滤处理一下

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -9,7 +9,7 @@
                 <span class="movie-rating">{{movie.Rated}}</span>
             </div>
             <div class="movie-sessions">
-                <div class="session-time-wrapper" v-for="session in sessions">
+                <div class="session-time-wrapper" v-for="session in filteredSession(sessions)">
                     <div class="session-time">{{formatSessionTime(session.time)}}</div>
                 </div>
             </div>
@@ -24,6 +24,9 @@
         methods: {
             formatSessionTime(rawTime) {
                 return this.$moment(rawTime).format('h:mm A');
+            },
+            filteredSession(sessions){
+                return sessions;
             }
         }
     }
