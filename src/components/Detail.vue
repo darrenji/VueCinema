1db2c654e997f4c5eb66568c23bdd41cff2c552c
darren <764190362@qq.com>
2017-11-03 23:13:58 +0800

    完成Detail组件，好写总结了

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,22 +1,31 @@
 <template>
-    <div id="detail" v-if="movie">
-        <movie-item v-bind:movie="movie.movie"></movie-item>
+    <div v-if="movie" id="detail">
+        <movie-item v-bind:movie="movie">
+            <div class="movie-details">
+                <p class="movie-genre">{{ movie.Genre }}</p>
+                <p class="movie-plot">{{ movie.Plot }}</p>
+                <table>
+                    <tr><td>Released date: </td><td>{{ movie.Released }}</td></tr>
+                    <tr><td>Running time: </td><td>{{ movie.Runtime }}</td></tr>
+                    <tr><td>Director: </td><td>{{ movie.Director }}</td></tr>
+                    <tr><td>Cast: </td><td>{{ movie.Actors }}</td></tr>
+                </table>
+            </div>
+        </movie-item>
+        <div class="home">
+            <router-link v-bind:to="{ name: 'home' }">Back to results</router-link>
+        </div>
     </div>
 </template>
-
-
 <script>
     import MovieItem from './MovieItem.vue';
     export default {
-        props: ['movies'],
+        props: [ 'movies' ],
         computed: {
-            movie(){
-               let movie =  this.movies.find(movie => movie.id === this.$route.params.id);
-                return movie ? movie : null;
+            movie() {
+                let movie = this.movies.find(movie => movie.id === this.$route.params.id);
+                return movie ? movie.movie : null;
             }
-        },
-        created(){
-            console.log(this.$route);
         },
         components: {
             MovieItem
