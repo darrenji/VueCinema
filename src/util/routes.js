cf3a65952aaa4c011f41ccf5e06edbda08437a9a
darren <764190362@qq.com>
2017-11-03 22:07:09 +0800

    添加一个路由，指向Detail组件

--- src/util/routes.js
+++ src/util/routes.js
@@ -1,4 +1,6 @@
 import Overview from '../components/Overview.vue';
+import Detail from '../components/Detail.vue';
 export default [
-    {path: '/', component: Overview}
+    {path: '/', component: Overview},
+    {path: '/movie', component: Detail}
 ];
