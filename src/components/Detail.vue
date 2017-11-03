6d772f795837eaa682a6a68cc0df580ded435572
darren <764190362@qq.com>
2017-11-03 22:44:49 +0800

    显示Detail详情，通过一个computed属性来获取到某个movie,逻辑是从集合中找出一个元素的id与路由对象中的params.id一致

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,13 +1,25 @@
 <template>
-    <div>detail</div>
+    <div id="detail" v-if="movie">
+        <movie-item v-bind:movie="movie.movie" v-bind:sessions="[]"></movie-item>
+    </div>
 </template>
 
 
 <script>
+    import MovieItem from './MovieItem.vue';
     export default {
         props: ['movies'],
+        computed: {
+            movie(){
+               let movie =  this.movies.find(movie => movie.id === this.$route.params.id);
+                return movie ? movie : null;
+            }
+        },
         created(){
             console.log(this.$route);
+        },
+        components: {
+            MovieItem
         }
     }
 </script>
