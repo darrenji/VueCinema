76cb956bd4dc072b5302389f1edf6c586da3b5f3
darren <764190362@qq.com>
2017-11-03 13:47:01 +0800

    在MovieItems上添加过滤

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -4,7 +4,9 @@
              <movie-item v-for="movie in filteredMovies" 
                  v-bind:movie="movie.movie" 
                  v-bind:day = "day"
-                 v-bind:sessions="movie.sessions"></movie-item>
+                 v-bind:sessions="movie.sessions"
+                 v-bind:time="time"
+                 ></movie-item>
          </div>
          <div v-else-if="movies.length" class="no-results">
              No results.
@@ -23,35 +25,32 @@
     export default {
             props: ['genre', 'time', 'movies', 'day'],
             methods: {
-              moviePassesGenreFilter(movie){
-                  if(!this.genre.length){
-                      return true;
-                  } else {
-                      //获取当前movie中的所有genre
-                      let movieGenres = movie.movie.Genre.split(", ");
-                      
-                      let matched = true;                    
-                      this.genre.forEach(genre => {
-                          if(movieGenres.indexOf(genre) === -1){
-                              matched = false;
-                          }
-                      });
-                      return matched;
-                  }
-                  
-              },
-              sessionPassesTimeFilter(session){
-                  if(!this.day.isSame(this.$moment(session.time), 'day')) {
-                      return false;
-                  } else if (this.time.length ===0 || this.time.length ===2) {
-                      return true;
-                  } else if (this.time[0] === times.AFTER_6PM){
-                      return this.$moment(session.time).hour() >= 18;
-                  } else {
-                      return this.$moment(session.time).hour() < 18;
-                  }
-                  
-              }
+              moviePassesGenreFilter(movie) {
+                if (!this.genre.length) {
+                    return true;
+                } else {
+                    let movieGenres = movie.movie.Genre.split(", ");
+                    let matched = true;
+                    this.genre.forEach(genre => {
+                        if (movieGenres.indexOf(genre) === -1) {
+                            matched = false;
+                        }
+                    });
+                    return matched;
+                }
+            },
+            sessionPassesTimeFilter(session) {
+                if (!this.day.isSame(this.$moment(session.time), 'day')) {
+                    return false;
+                } else if (this.time.length === 0 || this.time.length === 2) {
+                    return true;
+                } else if (this.time[0] === times.AFTER_6PM) {
+                    return this.$moment(session.time).hour() >= 18;
+                } else {
+                    return this.$moment(session.time).hour() < 18;
+                }
+            }
+              
             },
             computed: {
                 filteredMovies(){
