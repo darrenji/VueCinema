8dffbba728b171f9ded642f497a7833e8502e91a
darren <764190362@qq.com>
2017-11-03 22:10:41 +0800

    给路由一个name属性，然后通过router-link导向到某个组件

--- src/util/routes.js
+++ src/util/routes.js
@@ -1,6 +1,6 @@
 import Overview from '../components/Overview.vue';
 import Detail from '../components/Detail.vue';
 export default [
-    {path: '/', component: Overview},
-    {path: '/movie', component: Detail}
+    {path: '/', component: Overview, name: 'home'},
+    {path: '/movie', component: Detail,name:'movie'}
 ];
